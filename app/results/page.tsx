c9d1d63134c42bbddab2a8a407d91c2db22d4e1a
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-10-27 10:33:03 +0300

    implement footer

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -135,6 +135,12 @@ export default function ResultsPage() {
           </div>
         </CardContent>
       </Card>
+      <footer className="text-center text-gray-500 mt-8 flex items-center justify-center gap-2">
+        <img src="/gdg-logo.png" alt="GDG Quiz" className="w-10 h-10" />
+        <p className="text-md ">
+              Built by<a href="https://github.com/DogukanUrker/GDGQuiz" target="_blank" className="text-blue-600 hover:text-blue-700 font-bold"> Software Team</a>
+        </p>
+      </footer>
     </main>
   );
 }
